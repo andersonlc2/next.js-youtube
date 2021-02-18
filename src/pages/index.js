7424d8decc90a1032f6e3ff4f95e876959a79d9b
Anderson <acorreia610@gmail.com>
2021-02-18 16:02:47 -0300

    Layout Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
+import Layout from '../components/Layout';
+
 export default function Home() {
   return (
-  <div>
+  <Layout title='YouTube'>
     Clone YouTube com Next.js + Material-UI
-    <Button variant="outlined" color="secondary">Hello World</Button>
-  </div>
+  </Layout>
   )
 }
